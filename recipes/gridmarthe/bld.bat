586551676bf9d580f09b58184ad015d22f152daa
manlaya <a.manlay@brgm.fr>
2024-11-14 15:35:18 +0100

    update metadata file and build scripts for gridmarthe

--- recipes/gridmarthe/bld.bat
+++ recipes/gridmarthe/bld.bat
@@ -1,8 +1,7 @@
+:: Windows build script
+cd %SRC_DIR%\src\gridmarthe\lecsem
+%PYTHON% setup.py build_ext --inplace --fcompiler=gnu95 -f
 
-cd src/gridmarthe/lecsem
-python setup.py build_ext --inplace --fcompiler=gnu95 -f
-
-cd ../../../
-python -m pip install --no-deps --ignored-installed -vv .
-
+cd %SRC_DIR%
+%PYTHON% -m pip install --no-deps --ignored-installed -vv .
 
--- recipes/gridmarthe/build.sh
+++ recipes/gridmarthe/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-cd src/gridmarthe/lecsem
-f2py -c lecsem.f90 edsemigl.f90 scan_grid.f90 -m lecsem --fcompiler=gfortran
+cd $SRC_DIR/src/gridmarthe/lecsem
+$PYTHON -m numpy.f2py -c lecsem.f90 edsemigl.f90 scan_grid.f90 -m lecsem --fcompiler=gfortran
 
-cd ../../../
+cd $SRC_DIR
 # no-deps : already installed with conda, do not try hell...
-python -m pip install --no-deps --ignored-installed -vv .
+$PYTHON -m pip install --no-deps --ignored-installed -vv .
 
 
