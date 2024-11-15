a12f3205bb356f37cfe3c47b2ea0ecaf91e60e67
manlaya <a.manlay@brgm.fr>
2024-11-15 12:43:09 +0100

    switch to meson backend for f2py: avoid distutils and then setuptools error, try to fix compilation for both linux/windows

--- recipes/gridmarthe/build.sh
+++ recipes/gridmarthe/build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 cd $SRC_DIR/src/gridmarthe/lecsem
-$PYTHON -m numpy.f2py -c lecsem.f90 edsemigl.f90 scan_grid.f90 -m lecsem --fcompiler=gfortran
+gfortran --version || { echo "gfortran not available"; exit 1; }
+CC=gcc FC=gfortran FFLAGS='-fdefault-real-8' $PYTHON -m numpy.f2py -c lecsem.f90 edsemigl.f90 scan_grid.f90 -m lecsem --backend=meson --lower
 
+echo "\n\n\n COMPILATION DONE \n\n\n"
 cd $SRC_DIR
 # no-deps : already installed with conda, do not try hell...
-$PYTHON -m pip install --no-deps --ignored-installed -vv .
+$PYTHON -m pip install -v . 
+# --no-deps -v
 
 
